03114393094f622b7a4f478005a4818dd8fdd307
Dara Fontoura <>
2021-06-06 20:27:16 +0000

    Em JSON

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ let apresentacao = {
 
 }
 console.log(JSON.stringify(apresentacao));
+
+//Em JSON
+
+{
+	"nome_idade": ["Dara Fontoura da Silva", "20 anos"],
+	"data_nascimento": "21 de Julho de 2000",
+	"ser_bluemer": "Desejo ser uma Bluemer, pois amo tecnologia e tenho muita vontade de aprender coisas novas. Quero muito poder trabalhar com algo que no qual sou tão apaixonada !"
+}
